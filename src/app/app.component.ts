0fe8db816d64fdbb52042cb7260e994454073303
mundo316 <raymundo.garcia316@yahoo.com>
2020-01-23 18:07:09 -0600

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,31 @@
 import { Component } from '@angular/core';
 import { IVehicleList } from './interfaces/car-list';
 
-@Component({
+Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+const vehicleList: IVehicleList[] = [
+  {
+    make: "Chevrolet",
+    model: "Silverado",
+    domestic: true,
+    sold: 32749,
+  },
+  {
+    make: "Ford",
+    model: "F-150",
+    domestic: true,
+    sold: 38732,
+  },
+  {
+    make: "Honda",
+    model: "Civic",
+    domestic: false,
+    sold: 98322,
+  }]
+
 export class AppComponent {
-vehicleList: IVehicleList[] = [
-{
-  make: "Chevrolet",
-  model: "Silverado",
-  domestic: true,
-  sold: 32749,
-},
-{
-  make: "Ford",
-  model: "F-150",
-  domestic: true,
-  sold: 38732,
-},
-{
-  make: "Honda",
-  model: "Civic",
-  domestic: false,
-  sold: 98322,
-}
-
-]
-
   title = 'MaterialTable';
 }
